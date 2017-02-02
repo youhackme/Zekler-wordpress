fa8751fb5ee23d23b6ebc079a67296f03534b054
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-02-02 07:45:12 +0400

    Enqueue style and script

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 function ZeklerScript()
 {
     wp_enqueue_style('zeklerStyle', get_bloginfo('template_directory') . '/build/css/style.min.css');
-    wp_enqueue_script("Zeklerscript", get_bloginfo('template_directory') . '/build/js/all.js', false, '1', true);
+    wp_enqueue_script("Zeklerscript", get_bloginfo('template_directory') . '/build/js/all.min.ssh hyder@163.172.90.99js', false, '1', true);
 }
 
 add_action('wp_enqueue_scripts', 'ZeklerScript');
