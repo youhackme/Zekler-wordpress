04e86fc1410143cbae91bc3c90839e9c5cc0a74f
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-02-05 23:15:40 +0400

    Remove thumbnail dimension to make it responsive

--- functions.php
+++ functions.php
@@ -1,10 +1,18 @@
 <?php
 
+add_action('wp_enqueue_scripts', 'ZeklerScript');
 
 function ZeklerScript()
 {
     wp_enqueue_style('zeklerStyle', get_bloginfo('template_directory') . '/build/css/style.min.css');
-    wp_enqueue_script("Zeklerscript", get_bloginfo('template_directory') . '/build/js/all.min.ssh hyder@163.172.90.99js', false, '1', true);
+    wp_enqueue_script("Zeklerscript", get_bloginfo('template_directory') . '/build/js/all.min.js', false, '1', true);
 }
 
-add_action('wp_enqueue_scripts', 'ZeklerScript');
+
+add_filter('post_thumbnail_html', 'remove_thumbnail_dimensions', 10, 3);
+
+function remove_thumbnail_dimensions($html, $post_id, $post_image_id)
+{
+    $html = preg_replace('/(width|height)=\"\d*\"\s/', "", $html);
+    return $html;
+}
