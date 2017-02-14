f7a2b31d670b44805e84479c58bc8447029d832c
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-02-14 08:06:24 +0400

    Added default page template

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -1,22 +1,21 @@
 <div class="container share-widget">
-
     <main class="content col-md-8 bigger-col-md-8">
         <header>
             <div class="row meta-block">
                 <div class="col-md-1">
-                    <img src="<?php echo get_bloginfo('template_directory'); ?>/build/img/hyder.jpeg"
+                    <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/build/img/hyder.jpeg"
                          alt="profile picture" class="img-circle">
                 </div>
                 <div class="col-md-11">
                     <p class="meta-block__author-name">Hyder Abbass</p>
                     <p class="meta-block__other text-muted">
-                        <time datetime="<?php the_modified_time('Y-m-d'); ?>"><span><?php the_modified_time('F jS, Y'); ?>
+                        <time datetime="<?php the_modified_time( 'Y-m-d' ); ?>"><span><?php the_modified_time( 'F jS, Y' ); ?>
 </span></time>
                         <span class="middotDivider"></span>
                         <span>Windows</span>
                         <span class="middotDivider"></span>
-                        <span><i class=" icon-comment-o" aria-hidden="true"></i> <?php comments_number(0, 1,
-                                '%'); ?></span>
+                        <span><i class=" icon-comment-o" aria-hidden="true"></i> <?php comments_number( 0, 1,
+								'%' ); ?></span>
                         <span class="middotDivider"></span>
                         <span><i class=" icon-view" aria-hidden="true"></i> 2k</span>
                     </p>
@@ -24,16 +23,16 @@
             </div>
         </header>
         <article class="article-block">
-            <?php
+			<?php
 
-            the_title('<h1 class="entry-title">', '</h1>');
+			the_title( '<h1 class="entry-title">', '</h1>' );
 
-            the_content(sprintf(
-                __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen'),
-                get_the_title()
-            ));
-            $postID = get_the_ID();
-            ?>
+			the_content( sprintf(
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
+				get_the_title()
+			) );
+			$postID = get_the_ID();
+			?>
         </article>
         <footer>
             <div class="row">
@@ -47,7 +46,7 @@
             <div class="row">
                 <div class="biography-block">
                     <div class="col-md-4">
-                        <img src="<?php echo get_bloginfo('template_directory'); ?>/build/img/hyder.jpeg"
+                        <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/build/img/hyder.jpeg"
                              alt="author photo" class="img-circle">
                         <span class="text-muted">Hyder Abbass</span>
                     </div>
@@ -56,13 +55,13 @@
             </div>
         </footer>
     </main>
-    <?php get_sidebar(); ?>
+	<?php get_sidebar(); ?>
 </div>
 <div class="container-fluid darker-bg">
     <div class="container">
         <div class="row">
             <div class="related-main-block">
-                <?php echo do_shortcode('[manual_related_posts]'); ?>
+				<?php echo do_shortcode( '[manual_related_posts]' ); ?>
             </div>
         </div>
 
@@ -121,30 +120,30 @@
                 </div>
 
 
-                <?php
-                $args = [
-                    'status'  => 'approve',
-                    'post_id' => $postID,
-                    'type'    => 'comment',
-                ];
+				<?php
+				$args = [
+					'status'  => 'approve',
+					'post_id' => $postID,
+					'type'    => 'comment',
+				];
 
 
-                // The Query
-                $comments_query = new WP_Comment_Query;
-                $comments = $comments_query->query($args);
+				// The Query
+				$comments_query = new WP_Comment_Query;
+				$comments       = $comments_query->query( $args );
 
-                // Comment Loop
-                if ($comments) {
-                    foreach ($comments as $comment) {
-                        ?>
+				// Comment Loop
+				if ( $comments ) {
+					foreach ( $comments as $comment ) {
+						?>
                         <div class="response-block response-id-<?php echo $comment->comment_ID; ?>">
                             <div class="row">
                                 <div class="response-block__wrapper">
                                     <div class="response-block__meta">
                                         <div class="col-md-1">
                                             <img width="40" height="40"
-                                                 src="<?php echo get_avatar_url($comment->comment_author_email,
-                                                     ['size' => '40']); ?>"
+                                                 src="<?php echo get_avatar_url( $comment->comment_author_email,
+												     [ 'size' => '40' ] ); ?>"
                                                  alt="<?php echo $comment->comment_author; ?>'s photo"
                                                  class="img-circle">
                                         </div>
@@ -153,25 +152,25 @@
                                             <p class="response-block__other text-muted">
                                                 <span>
                                                     <small>
-                                                        <?php echo (new DateTime($comment->comment_date))->format('l, F jS, Y'); ?>
+                                                        <?php echo ( new DateTime( $comment->comment_date ) )->format( 'l, F jS, Y' ); ?>
                                                     </small>
                                                 </span>
                                             </p>
                                         </div>
                                     </div>
                                     <div class="response-block__content">
-                                        <?php echo $comment->comment_content; ?>
+										<?php echo $comment->comment_content; ?>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <?php
+						<?php
 
-                    }
-                } else {
-                    echo 'No comments found.';
-                }
-                ?>
+					}
+				} else {
+					echo 'No comments found.';
+				}
+				?>
 
 
             </div>
